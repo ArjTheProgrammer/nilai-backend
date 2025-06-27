d4e267679ff2469bb6c9b274431ba8216b91555b
Jose Neil Silagan <neilsilagan@gmail.com>
2025-06-27 19:23:24 +0800

    Change the json output so that it will be possible to identify multiple emotions in the journal

--- utils/services/emotion.js
+++ utils/services/emotion.js
@@ -2,7 +2,7 @@ const axios = require('./axios.js')
 const baseUrl = '/emotions'
 
 const getEmotion = async () => {
-  const response = await axios.post(baseUrl, { text: 'ang tanong' })
+  const response = await axios.post(baseUrl, { text: 'I am happy today!' })
   return response.data
 }
 
